93ab70e6cd7f421d241ceb5cd85bbe7689bad50f
TooFooDev <144743612+toofoodev@users.noreply.github.com>
2026-03-01 19:04:31 -0600

    main

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+let onlineUsers = 0;
+
+app.get("/track", (req, res) => {
+    onlineUsers++;
+    console.log("Online Users:", onlineUsers);
+});
+
+app.get("/count", (req, res) => {
+    res.json({ onlineUsers });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
